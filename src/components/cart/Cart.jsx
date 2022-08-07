8613c2ac5586c9ac168fb53271f107a7652da46b
sebagonzalez <gabrielsebastian@gmail.com>
2022-08-07 17:47:38 -0300

    item collection II

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,12 +1,34 @@
 import { useContext } from "react"
 import { cartContext } from "../../context/cartContext"
 import { Link } from 'react-router-dom'
+import { addDoc, collection, getFirestore } from "firebase/firestore"
 
 
 
 const Cart = () => {
 
     const { cart, deleteOne, deleteAll, suma } = useContext(cartContext)
+
+    const createOrder = async (e) => {
+        e.preventDefault();
+        const db = getFirestore();
+        const orderCollectionQuery = collection(db, 'orders');
+        const order = {
+            buyer: {name: 'Tomas' , phone: '1212334455', email: 'test@gmail.com'},
+            items: cart.map((product) => ({
+                name: product.nombre,
+                price: product.precio,
+            })),
+            total: cart.reduce((acc, product) => acc + product.precio * product.cantidad, 0 ),
+        };
+        await addDoc(orderCollectionQuery, order)
+        .then((response) => {
+            console.log(response.id);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
 
     if (cart.length === 0) {
         return <h3>No hay productos, volver al <Link to='/'>Home</Link></h3>
@@ -35,6 +57,9 @@ const Cart = () => {
         </div>
         <div>
             <h3>Total: ${suma}</h3>
+        </div>
+        <div>
+            <button onClick={createOrder}>Crear Orden</button>
         </div>
     </div>
   )
